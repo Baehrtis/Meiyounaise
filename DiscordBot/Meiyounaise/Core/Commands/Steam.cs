cad19fd77b4feb2ca06bdbe37a6a0d7cc24237c3
Unknown <timo.nock@gmx.de>
2018-09-13 23:52:13 +0200

    Cleanup &steam

--- DiscordBot/Meiyounaise/Core/Commands/Steam.cs
+++ DiscordBot/Meiyounaise/Core/Commands/Steam.cs
@@ -15,45 +15,64 @@ namespace Meiyounaise.Core.Commands
         private SteamUser sInterface;
         private string key = "";
 
+        private void getKey()
+        {
+            using (var Stream = new FileStream((Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Replace(@"bin\Debug\netcoreapp2.1", @"Data\SteamKey.txt"), FileMode.Open, FileAccess.Read))
+            using (var ReadToken = new StreamReader(Stream))
+            {
+                key =  ReadToken.ReadToEnd();
+            }
+        }
         private Embed buildSteamEmbed(ulong id)
         {
+            string curl = sInterface.GetCommunityProfileAsync(id).Result.CustomURL;
+            if (curl == "")
+            {
+                curl = "Not set!";
+            }
             EmbedBuilder steamEmbed = new EmbedBuilder()
                 .WithAuthor(author =>
                 {
                     author
                         .WithName(sInterface.GetPlayerSummaryAsync(id).Result.Data.Nickname)
                         //.WithIconUrl(sInterface.GetCommunityProfileAsync(id).Result.AvatarMedium.AbsoluteUri)
-                        .WithUrl($"https://steamcommunity.com/profiles/{id}/");
+                        .WithUrl(sInterface.GetPlayerSummaryAsync(id).Result.Data.ProfileUrl);
                 })
                 .WithThumbnailUrl(sInterface.GetCommunityProfileAsync(id).Result.AvatarFull.AbsoluteUri)
                 .AddField("steamID64", id)
-                .AddField("Custom URL", sInterface.GetPlayerSummaryAsync(id).Result.Data.Nickname);
+                .AddField("Custom URL", curl);
             return steamEmbed.Build();
         }
 
         [Command("steam")]
         public async Task steam(string idname)
         {
-            using (var Stream = new FileStream((Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Replace(@"bin\Debug\netcoreapp2.1", @"Data\SteamKey.txt"), FileMode.Open, FileAccess.Read))
-            using (var ReadToken = new StreamReader(Stream))
-            {
-                key = ReadToken.ReadToEnd();
-            }
+            getKey();
             sInterface = new SteamUser(key);
             ulong id = sInterface.ResolveVanityUrlAsync(idname).Result.Data;
-            await ReplyAsync("", false, buildSteamEmbed(id));
+            try
+            {
+                await ReplyAsync("", false, buildSteamEmbed(id));
+            }
+            catch (Exception e)
+            {
+                await ReplyAsync(e.Message);
+            }
         }
 
         [Command("steam")]
         public async Task steam(ulong id)
         {
-            using (var Stream = new FileStream((Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Replace(@"bin\Debug\netcoreapp2.1", @"Data\SteamKey.txt"), FileMode.Open, FileAccess.Read))
-            using (var ReadToken = new StreamReader(Stream))
-            {
-                key = ReadToken.ReadToEnd();
-            }
+            getKey();
             sInterface = new SteamUser(key);
-            await ReplyAsync("", false, buildSteamEmbed(id));
+            try
+            {
+                await ReplyAsync("", false, buildSteamEmbed(id));
+            }
+            catch (Exception e)
+            {
+                await ReplyAsync(e.Message);
+            }
         }
     }
 }
